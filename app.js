78bf68ba610de90e4109744e41943be4208b6a30
Maricel <maricelgotiscarciller29@gmail.com>
2022-10-20 23:59:48 +0800

    added add.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const add = require('./add')
 const write = require('./write')
 const read = require('./read')
 const present = require('./present')
+
+const cmd = process.argv
+const data = process.argv
+
+var note = {}
+
+if(data[2] == 'add') {
+   
+    note = {
+        id: data[3],
+        title: data[4],
+        body: data[5]
+    }
+   
+    var oldNote = read()
+   
+    add(note, oldNote);
+}
+if(data[2] == 'read') {
+    
+    const present = require('./present')
+
+    present(read())
+}
